1eb4e7e8ec27cbd030cda50d7f10c9c8a6fe7759
Malikfasih <90088021+Malikfasih@users.noreply.github.com>
2024-03-30 23:17:26 +0500

    added and config env variables

--- db.js
+++ db.js
@@ -2,8 +2,6 @@ import mysql from "mysql2/promise";
 import * as dotenv from "dotenv";
 
 dotenv.config();
-console.log("database name:", process.env.DATABASE_NAME);
-console.log("stackscribe_db");
 
 export const db = await mysql.createConnection({
   host: "localhost",
