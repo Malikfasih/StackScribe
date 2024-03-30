38587d735350f684170a4f5466af64af79782b31
Malikfasih <90088021+Malikfasih@users.noreply.github.com>
2024-03-30 23:09:47 +0500

    defined the post route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRouter from "./routes/auth.js";
+import postRouter from "./routes/post.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -12,3 +13,4 @@ app.listen(8000, () => {
 });
 
 app.use("/user", authRouter);
+app.use("/posts", postRouter);
