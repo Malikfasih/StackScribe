254822333bb7f06d33cfe3dfd6459369426c883c
Malikfasih <90088021+Malikfasih@users.noreply.github.com>
2024-03-30 23:13:54 +0500

    implement Jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,29 +1,8 @@
 import bcrypt from "bcrypt";
 import { db } from "../db.js";
+import jwt from "jsonwebtoken";
 
-// Register user
-// export const register = (req, res) => {
-//   const { username, email, password } = req.body;
-//   // Check existing user
-//   const q = "SELECT * FROM users WHERE username = ? OR email = ?";
-
-//   db.query(q, [username, email], (err, data) => {
-//     if (err) return res.status(500).json(err);
-//     if (data.length) return res.status(409).send("User already exists");
-
-//     console.log("plain password:", password);
-//     bcrypt.hash(password, 10, (hashErr, hashPassword) => {
-//       if (hashErr) res.status(500).json(hashErr);
-//       console.log("hashed password:", hashPassword);
-//       const q = "INSERT INTO users(`username`, `email`, `password`) VALUES (?)";
-//       db.query(q, [username, email, hashPassword], (err, data) => {
-//         if (err) return res.json(err);
-//         return res.status(200).json("User has been created.");
-//       });
-//     });
-//   });
-// };
-
+// Register User
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
 
@@ -56,13 +35,56 @@ export const register = async (req, res) => {
     ]);
 
     return res.status(200).json("User has been created.");
-  } catch (err) {
-    return res.status(500).json(err);
+  } catch (error) {
+    return res.status(500).json(error);
   }
 };
 
 // Login user
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  try {
+    // Check user exists
+    const userExists = await db.query("SELECT * FROM users WHERE email = ?", [
+      req.body.email,
+    ]);
+    if (!userExists[0][0]) res.status(404).json("User not found!");
+
+    //  Check password is correct
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      userExists[0][0].password
+    );
+    if (!isPasswordCorrect) res.status(404).json("Invalid credentials");
+
+    // Generate token
+    const token = jwt.sign(
+      { email: userExists[0][0].email, id: userExists[0][0]._id },
+      process.env.JWT_SECRET_KEY
+    );
+
+    // separate password from user credentials
+    const { password, ...other } = userExists[0][0];
+    console.log("other fields", other);
+    console.log("user password", password);
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(other);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 // Logout user
-export const logout = async (req, res) => {};
+export const logout = async (req, res) => {
+  res
+    .clearCookie("access_token", {
+      sameSite: "none",
+      secure: true,
+    })
+    .status(200)
+    .json("User has been logged out.");
+};
